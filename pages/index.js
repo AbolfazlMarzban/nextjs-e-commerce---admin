37a92905292f3eda906431805c2292e9300bd346
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-05-10 18:35:27 +0430

    abol 1402-2-20

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const {data: session} = useSession();
   return (
    <Layout>
-    test
+    <div className="text-blue-900"></div>
+    Hello Abolfazl
    </Layout>
   )
 }
