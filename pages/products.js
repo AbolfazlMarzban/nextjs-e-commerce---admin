888a20855787ba60ca6c0d5aa1b7d5872c29a407
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-05-10 18:44:04 +0430

    abol 1402-2-20

--- pages/products.js
+++ pages/products.js
@@ -1,7 +1,10 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products(){
     return(
-        <Layout>Products</Layout>
+        <Layout>
+            <Link className="bg-gray-200 py-1 px-2" href={'/products/new'}>Add new product</Link>
+        </Layout>
     )
 }
