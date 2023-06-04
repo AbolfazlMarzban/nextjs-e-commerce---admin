7159fabe9b0ecc5951af8f6ba9e044f5fbbacb73
AbolfazlMarzban <abolfazlmarzban39@gmail.com>
2023-06-04 15:07:34 +0330

    abol 1402-3-13

--- components/productForm.js
+++ components/productForm.js
@@ -2,51 +2,93 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-export default function ProductForm ({
-    _id,
-    title:existingTitle,
-    description: existingDescription,
-    price: existingPrice,
-    images,
-}){
-    const router = useRouter()
-    const [title, setTitle] = useState( existingTitle ||'')
-    const [description, setDescription] = useState(existingDescription ||'')
-    const [price, setPrice] = useState(existingPrice ||'')
-    const [goToProducts, setGoToProducts] = useState(false)
-    async function saveProduct(ev){
-        ev.preventDefault();
-        const data = {title, description, price}
-        if(_id){
-            //update
-            await axios.put('/api/products', {...data, _id})
-        } else {
-            //create
-        await axios.post('/api/products', data)
-        }
-        setGoToProducts(true)
+export default function ProductForm({
+  _id,
+  title: existingTitle,
+  description: existingDescription,
+  price: existingPrice,
+  images,
+}) {
+  const router = useRouter();
+  const [title, setTitle] = useState(existingTitle || "");
+  const [description, setDescription] = useState(existingDescription || "");
+  const [price, setPrice] = useState(existingPrice || "");
+  const [goToProducts, setGoToProducts] = useState(false);
+  async function saveProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+    if (_id) {
+      //update
+      await axios.put("/api/products", { ...data, _id });
+    } else {
+      //create
+      await axios.post("/api/products", data);
     }
-    if(goToProducts){
-     router.push("/products")
+    setGoToProducts(true);
+  }
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const response = await axios.post("/api/upload", data, {
+        headers: { "Content-Type": "multipart/formdata" },
+      });
+      console.log(response);
     }
-        return(
-            <form onSubmit={ saveProduct }>
-            <label>Product name</label>
-           <input type="text" placeholder="product name" 
-           value={title} onChange={ev => setTitle(ev.target.value)}></input>
-           <label>Photos</label>
-           <div>
-            {!images?.length && (
-                <div>No Photos in this Product</div>
-            )}
-           </div>
-           <label>description</label>
-            <textarea placeholder="description" 
-            value={description} onChange={ev => setDescription(ev.target.value)}></textarea>
-            <label>Price (in USD)</label>
-            <input type="number" placeholder="price" 
-            value={price} onChange={ev => setPrice(ev.target.value)}></input>
-            <button type="submit" className="btn-primary">Save</button>
-            </form>
-    );
+  }
+  if (goToProducts) {
+    router.push("/products");
+  }
+  return (
+    <form onSubmit={saveProduct}>
+      <label>Product name</label>
+      <input
+        type="text"
+        placeholder="product name"
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      ></input>
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="mb-2 cursor-pointer w-24 h-24 text-center flex flex-col items-center justify-center text-sm gap-1 text-gray-600 rounded-md bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" className="hidden" onChange={uploadImages} />
+        </label>
+        {!images?.length && <div>No Photos in this Product</div>}
+      </div>
+      <label>description</label>
+      <textarea
+        placeholder="description"
+        value={description}
+        onChange={(ev) => setDescription(ev.target.value)}
+      ></textarea>
+      <label>Price (in USD)</label>
+      <input
+        type="number"
+        placeholder="price"
+        value={price}
+        onChange={(ev) => setPrice(ev.target.value)}
+      ></input>
+      <button type="submit" className="btn-primary">
+        Save
+      </button>
+    </form>
+  );
 }
